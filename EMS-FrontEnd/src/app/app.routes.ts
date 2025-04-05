26b1731eae0847eb3e27fa82b77cf0d049020ef9
Avinash Suryawanshi <suryawanshiavinash18@gmail.com>
2025-04-05 19:11:17 +0530

    create Side nav for frontend

--- EMS-FrontEnd/src/app/app.routes.ts
+++ EMS-FrontEnd/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: 'login'},
     {path: 'login', loadComponent: () => import('../app/components/user/login/login.component').then((c)=> c.LoginComponent)},
     {path: 'forgot-password', loadComponent: () => import('../app/components/user/forgot-password/forgot-password.component').then((c)=> c.ForgotPasswordComponent)},
-    {path: 'admin-dashboard', loadComponent: () => import('../app/components/dashboard/admin-dashboard/admin-dashboard.component').then((c)=> c.AdminDashboardComponent)},
+    
+    {path: '', component: LayoutComponent, children: [
+        {path: 'dashboard', loadComponent: () => import('../app/components/pages/dashboard/dashboard.component').then((c)=> c.DashboardComponent)},
+        {path: 'employee-management', loadComponent: () => import('../app/components/pages/employee-management/employee-management.component').then((c)=> c.EmployeeManagementComponent)},
+        {path: 'leave-management', loadComponent: () => import('../app/components/pages/leave-management/leave-management.component').then((c)=> c.LeaveManagementComponent)},
+        {path: 'sales-management', loadComponent: () => import('../app/components/pages/sales-management/sales-management.component').then((c)=> c.SalesManagementComponent)},
+        {path: 'payroll-management', loadComponent: () => import('../app/components/pages/payroll-management/payroll-management.component').then((c)=> c.PayrollManagementComponent)},
+        {path: 'reports', loadComponent: () => import('../app/components/pages/reports/reports.component').then((c)=> c.ReportsComponent)},
+
+    ]}
 ];
